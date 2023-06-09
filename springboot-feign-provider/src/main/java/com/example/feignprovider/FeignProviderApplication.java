46fd0a02ce22f8870d45c47e0c4c0952c6f07053
chuck47286 <chuck472861399@163.com>
2023-06-09 10:18:52 +0800

    [feature] feign服务提供者 注册到eureka发现中心

--- springboot-feign-provider/src/main/java/com/example/feignprovider/FeignProviderApplication.java
+++ springboot-feign-provider/src/main/java/com/example/feignprovider/FeignProviderApplication.java
@@ -2,6 +2,7 @@ package com.example.feignprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -9,6 +10,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @Date 2023/5/26 9:54
  * @Version 1.0
  */
+
+@EnableEurekaClient
 @SpringBootApplication
 @EnableSwagger2
 public class FeignProviderApplication {
